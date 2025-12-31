fefe7be46eaf56694749d56383ac4d647f70707c
jtmb <james.branco@gmail.com>
2025-12-30 20:10:33 -0500

    updated build script

--- compose-up.sh
+++ compose-up.sh
@@ -1,5 +1,3 @@
-docker rm -f kali-bootstrap
-rm -rfv ~/dev/container-storage/kali-bootstrap
 mkdir -p ~/dev/container-storage/kali-bootstrap
 docker compose up --build -d
 docker exec -it kali-bootstrap bash
